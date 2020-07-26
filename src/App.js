8ae6e0a7b724be4b2d7c9a94628232570a5f4fe9
sammer119 <sammer119@users.noreply.github.com>
2020-07-26 12:26:32 +0100

    Update Code

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Shop from './Shop';
+import Nav from './Nav';
+import About from './About';
+
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+        <h1>App</h1>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/Shop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Shop() {
+    return (
+    <>
+        <h1>Shop Page</h1>
+    </>
+    );
+}
+
+export default Shop;

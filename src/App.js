eb42f41de9fdb4abf48d66e728111135d5969df7
sammer119 <sammer119@users.noreply.github.com>
2020-07-26 14:26:06 +0100

    Fixed Code

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from 'react';
 import Shop from './Shop';
 import Nav from './Nav';
 import About from './About';
-
+import Home from './Home';
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-      <div className="App">
-        <h1>App</h1>
-      </div>
+      <Router>
+          <div className="App">
+              <Nav />
+              <Route path='/' exact component={Home} />
+              <Route path='/about' component={About} />
+              <Route path='/shop' component={Shop} />
+          </div>
+      </Router>
+      
   );
 }
 
--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import './App.css';
+import { Link } from 'react-router-dom'
 
 function Nav() {
     return (
         <nav>
-            <h3>Logo</h3>
-            <ul>
-                <li>About</li>
-                <li>Shop</li>
+            <Link to='/'><h3>Home</h3></Link>
+            <ul className="nav-links">
+                <Link to='/about'>
+                    <li>About</li>
+                </Link>
+                <Link to='/shop'>
+                    <li>Shop</li>
+                </Link>
             </ul>
         </nav>
     );
